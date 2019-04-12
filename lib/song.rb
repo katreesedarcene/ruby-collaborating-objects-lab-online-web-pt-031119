c9c7e38ed1710b69e50d58ecca672a4ecc5176cd
Kat B <katreesebarnes@gmail.com>
2019-04-12 22:07:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,10 @@ class Song
     song = Song.new(filename)
     song = filename.split(" - ")[1]
     new_song_instance = self.new(song)
-    # new_song_instance.artist.name = artist
-    
-    
-    
+    new_song_instance.artist_name = artist
   end
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
