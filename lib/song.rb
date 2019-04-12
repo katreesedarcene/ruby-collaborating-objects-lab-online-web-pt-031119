27f5c669862008bff8a6e6f1236e84adabf5959d
Kat B <katreesebarnes@gmail.com>
2019-04-12 21:41:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
+require "pry"
 class Song 
+  attr_accessor :name, :artist 
   
+  def initialize(name)
+    @name = name
+  end
+  def self.new_by_filename(filename)
+    song = Song.new(filename)
+    song = filename.split(" - ")[1]
+  end
   
-  
-  
+
 end
